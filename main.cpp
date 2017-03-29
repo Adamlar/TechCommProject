5604dc310e6b7b7b15b5d21c2a6ca80f81442961
erensahin <sahineren.09@gmail.com>
2017-03-29 13:43:39 +0300

    Created class objects and edited main to run the algorithms. Also added a few datasets to Resources folder.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@
  */
 
 #include <cstdlib>
+#include "SortClient.h"
+#include "MergeSort.h"
+#include "BubbleSort.h"
 
 using namespace std;
 
@@ -19,7 +22,26 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
+	SortClient client;
+	vector<abstract_sort_algorithm*> algorithms;
+	std::string file_name = "Resources/1k-10k.txt";
 
+	MergeSort *m = new MergeSort();
+	BubbleSort *b = new BubbleSort();
+	/* add algorithms to container. */
+	algorithms.push_back(m);
+	algorithms.push_back(b);
+
+	client.read_data(file_name);
+
+	for (int i = 0; i < algorithms.size(); i++)
+	{
+		/* Strategy Pattern: set the strategy of the client as one of the derived sort algorithm class objects. */
+		client.setStrategy(algorithms[i]);
+		client.sort();
+		client.printRunningTime(file_name); /*create outputs/ folder if it does not exist. */
+		/*TODO: Add a method to export sorted data so we can check if the algorithms are implemented correctly or not. */
+	}
     return 0;
 }
 
